edce2dd8cb14fa417a07dd74d2abbfe183bfa8c3
ksfdesignuk-lang <ksfdesignuk@gmail.com>
2026-03-18 19:45:47 +0000

    Create index.js

---
+++ pages/index.js
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <div style={{padding: 20}}>
+      <h1>🌍 Welcome to Jaxon's World</h1>
+      <p>Your GCSE Adventure begins here!</p>
+    </div>
+  );
+}
